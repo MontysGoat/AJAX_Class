6131387a2cbb9dc980b89c1ba4d4d85d7e79117c
George Hearn <george.hearn@schneider-electric.com>
2019-03-09 14:24:34 -0500

    Module 6 initial commit

---
+++ Module6/app.js
@@ -0,0 +1,13 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController',  LunchCheckController);
+
+angular.injector($scope);
+
+function LunchCheckController ($scope) {
+  
+}
+
+})();

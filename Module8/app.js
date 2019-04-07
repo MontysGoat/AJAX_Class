5b60169f9d7074b39da70bb59f00c71d1827da58
George Hearn <george.hearn@schneider-electric.com>
2019-04-07 14:50:54 -0400

    Final commit Module 8

--- Module8/app.js
+++ Module8/app.js
@@ -1,74 +1,73 @@
 (function () {
-    'use strict';
-    
-    angular.module('NarrowItDownApp', [])
+  'use strict';
+
+  angular.module('NarrowItDownApp', [])
     .controller('NarrowItDownController', NarrowItDownController)
     .service('MenuSearchService', MenuSearchService)
-    .directive('foundItems', FoundItemsDirective);
-    
-    
-    function FoundItemsDirective() {
-      var ddo = {
-        templateUrl: 'shoppingList.html',
-        scope: {
-          items: '<',
-          myTitle: '@title',
-          onRemove: '&'
-        },
-        controller: FoundItemsDirectiveController,
-        controllerAs: 'list',
-        bindToController: true,
-        transclude: true
-      };
-    
-      return ddo;
-    }
-    
-    
-    
-    NarrowItDownController.$inject = ['MenuSearchService'];
-    function NarrowItDownController(MenuSearchService) {
-      var list = this;
-    
-      // Use factory to create new shopping list service
-      var shoppingList = ShoppingListFactory();
-    
-      list.items = shoppingList.getItems();
-      var origTitle = "Shopping List #1";
-      list.title = origTitle + " (" + list.items.length + " items )";
-    
-      list.warning = "COOKIES DETECTED!";
-    
-      list.itemName = "";
-      list.itemQuantity = "";
-    
-      list.addItem = function () {
-        shoppingList.addItem(list.itemName, list.itemQuantity);
-        list.title = origTitle + " (" + list.items.length + " items )";
-      };
-    
-      list.removeItem = function (itemIndex) {
-        console.log("'this' is: ", this);
-        this.lastRemoved = "Last item removed was " + this.items[itemIndex].name;
-        shoppingList.removeItem(itemIndex);
-        this.title = origTitle + " (" + list.items.length + " items )";
-      };
-    }
-    
-    
-    // If not specified, maxItems assumed unlimited
-    function MenuSearchService(maxItems) {
-      var service = this;
-    
-      // List of shopping items
-      var items = [];
+    .directive('foundItems', FoundItemsDirective)
+    .constant('ApiBasePath', "https://www.davidchuschinabistro.com");
 
-      service.getmatchedMenuItems = function (searchTerm){
 
+  function FoundItemsDirective() {
+    var ddo = {
+      templateUrl: 'foundItems.html',
+      scope: {
+        found: '<',
+        onRemove: '&',
+        nothingFound: '<'
+      },
+      controller: NarrowItDownController,
+      controllerAs: 'controller',
+      bindToController: true
+    };
+
+    return ddo;
+  }
+
+
+
+  NarrowItDownController.$inject = ['MenuSearchService'];
+  function NarrowItDownController(MenuSearchService) {
+    var controller = this;
+    controller.searchTerm = "";
+    controller.found = {};   
+
+    controller.narrowItDown = function () {
+      if(controller.searchTerm.trim() != ""){
+        MenuSearchService.getMatchedMenuItems(controller.searchTerm)
+        .then( function (result){
+          controller.found = result;
+        })
+      } else {
+        controller.found = [];
       }
-    
-     
     }
-    
-    })();
-    
+
+    controller.onRemove = function (itemIndex){
+      controller.found.splice(itemIndex, 1)
+    }
+
+    controller.nothingFound = function () {
+      return controller.found.length == 0 ;
+    }
+  }
+
+
+  MenuSearchService.$inject = ['$http', 'ApiBasePath', '$filter'];
+  function MenuSearchService($http, ApiBasePath, $filter) {
+    var service = this;
+
+    service.getMatchedMenuItems = function (searchTerm) {
+      return $http({
+        method: "GET",
+        url: (ApiBasePath + "/menu_items.json")
+      })
+      .then(function (result) {
+        var foundItems = $filter('filter')(result.data.menu_items, { description: searchTerm });
+        return foundItems;
+      })
+    }
+
+  }
+
+})();

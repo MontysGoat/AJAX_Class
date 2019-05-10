58c9f8383e3511b45280f6a301b105bb7e9bf1d7
George Hearn <george.hearn@schneider-electric.com>
2019-05-10 17:31:18 -0400

    array map fix

--- Module10/src/public/signup/signup.controller.js
+++ Module10/src/public/signup/signup.controller.js
@@ -7,7 +7,7 @@
   SignUpController.$inject = ['allMenuItems'];  
   function SignUpController(allMenuItems) {
     var $ctrl = this;
-    $ctrl.allMenuItems = allMenuItems.map(x => x.short_name);
+    $ctrl.allMenuItems = allMenuItems.menu_items.map(x => x.short_name);
     
     $ctrl.submit = function () {
       ctrl.completed = true;
